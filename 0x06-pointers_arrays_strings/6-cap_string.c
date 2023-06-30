957b2e7da8ed5e4b915c1786c35afc89f5dd4149
Divinia <divinialebo@gmail.com>
2023-06-30 16:12:37 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  *
  * Return: A pointer to the changed string.
  */
-char*cap_string(char*str)
+char *cap_string(char *str)
 {
 	int index = 0;
 
@@ -13,7 +13,6 @@ char*cap_string(char*str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
@@ -22,16 +21,15 @@ char*cap_string(char*str)
 		    str[index - 1] == '.' ||
 		    str[index - 1] == '!' ||
 		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
 		    str[index - 1] == '(' ||
 		    str[index - 1] == ')' ||
 		    str[index - 1] == '{' ||
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
+
 		index++;
 	}
 
 	return (str);
 }
-
